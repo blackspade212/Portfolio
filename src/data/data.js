5e0e3e344d87094e71d06d2846b535c6d1236475
D H E E R A J <118712121+blackspade212@users.noreply.github.com>
2023-12-15 02:07:13 +0530

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: './src/components/work/QUEST.png',
+    title: "DSA Quest Map",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/Bv75CLyN/4167022.jpg',
+    title: "Hostel Vibe",
+    link: "",
+    git: "",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/wBjcJcW7/5890625.jpg',
+    title: "Motron",
+    desc: "",
+    link: "",
+    git: "",
+  }
+  
+];
